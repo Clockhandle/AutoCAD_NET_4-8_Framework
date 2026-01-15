bbfd7d4c73f338aadeda015d4d8fc46aed512f7b
Clockhandle <155466922+Clockhandle@users.noreply.github.com>
2026-01-16 02:47:21 +0700

    made method to upload to server post.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.OnExportToCSV = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // OnSelectObject
@@ -86,6 +87,16 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.OnSelectObject_C);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(593, 387);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(187, 45);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "SendToServer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.OnSendToServer_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -93,6 +104,7 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(966, 486);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.OnExportToCSV);
@@ -112,5 +124,6 @@
         private System.Windows.Forms.Button OnExportToCSV;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -10,6 +10,10 @@ using System.Linq;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using Application = Autodesk.AutoCAD.ApplicationServices.Application;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace AutoCAD_NET_4_8_Framework
 {
     public partial class Form1 : Form
@@ -17,6 +21,7 @@ namespace AutoCAD_NET_4_8_Framework
         private List<ObjectId> _selectedObjectIds_A = new List<ObjectId>();
         private List<ObjectId> _selectedObjectIds_B = new List<ObjectId>();
         private List<ObjectId> _selectedObjectIds_C = new List<ObjectId>();
+        private static readonly HttpClient _client = new HttpClient();
         public Form1()
         {
             InitializeComponent();
@@ -63,113 +68,22 @@ namespace AutoCAD_NET_4_8_Framework
             }
 
             Document doc = Application.DocumentManager.MdiActiveDocument;
-            Database db = doc.Database;
 
+            // Create empty lists
             List<CADObjectData> cadObjectsData_A = new List<CADObjectData>();
             List<CADObjectData> cadObjectsData_B = new List<CADObjectData>();
             List<CADObjectData> cadObjectsData_C = new List<CADObjectData>();
+
             using (DocumentLock docLock = doc.LockDocument())
+            using (Transaction tr = doc.Database.TransactionManager.StartTransaction())
             {
-                using (Transaction tr = db.TransactionManager.StartTransaction())
-                {
-                    List<CADObjectData> ExtractDataFromLists(List<ObjectId> ids, string groupName)
-                    {
-                        List<CADObjectData> tempList = new List<CADObjectData>();
-                        foreach (ObjectId id in ids)
-                        {
-                            if (id.IsErased) continue;
-                            Entity ent = tr.GetObject(id, OpenMode.ForRead) as Entity;
-                            if (ent != null)
-                            {
-                                CADObjectData cadData = new CADObjectData
-                                {
-                                    GroupName = groupName,
-                                    Handle = ent.Handle.ToString(),
-                                    ObjectType = ent.GetType().Name,
-                                    Layer = ent.Layer
-                                };
-                                if (ent is Line line)
-                                {
-                                    cadData.StartPoint = new double[] { line.StartPoint.X, line.StartPoint.Y, line.StartPoint.Z };
-                                    cadData.EndPoint = new double[] { line.EndPoint.X, line.EndPoint.Y, line.EndPoint.Z };
-                                }
-                                else if (ent is Circle circle)
-                                {
-                                    cadData.CenterPoint = new double[] { circle.Center.X, circle.Center.Y, circle.Center.Z };
-                                    cadData.Radius = circle.Radius;
-                                }
-                                if (ent is Arc arc)
-                                {
-                                    cadData.CenterPoint = new double[] { arc.Center.X, arc.Center.Y, arc.Center.Z };
-                                    cadData.Radius = arc.Radius;
+                // LOOK HOW CLEAN THIS IS NOW:
+                // We use the same 'GetCadData' helper as the Upload button!
+                cadObjectsData_A = GetCadData(tr, _selectedObjectIds_A, "Group_A");
+                cadObjectsData_B = GetCadData(tr, _selectedObjectIds_B, "Group_B");
+                cadObjectsData_C = GetCadData(tr, _selectedObjectIds_C, "Group_C");
 
-                                    // AutoCAD angles are in Radians (0 to 2PI)
-                                    cadData.StartAngle = arc.StartAngle;
-                                    cadData.EndAngle = arc.EndAngle;
-                                    cadData.TotalAngle = arc.TotalAngle;
-                                }
-                                else if (ent is Polyline polyline)
-                                {
-                                    cadData.Vertices = new List<double[]>();
-                                    cadData.Bulges = new List<double>();
-
-                                    int vertCount = polyline.NumberOfVertices;
-                                    for (int i = 0; i < vertCount; i++)
-                                    {
-                                        Point3d pt = polyline.GetPoint3dAt(i);
-                                        cadData.Vertices.Add(new double[] { pt.X, pt.Y, pt.Z });
-
-                                        cadData.Bulges.Add(polyline.GetBulgeAt(i));
-                                    }
-                                    cadData.IsClosed = polyline.Closed;
-                                }
-                                else if (ent is Spline spline)
-                                {
-                                    if (spline.NumControlPoints > 0)
-                                    {
-                                        cadData.ControlPoints = new List<double[]>();
-                                        for (int i = 0; i < spline.NumControlPoints; i++)
-                                        {
-                                            Point3d cp = spline.GetControlPointAt(i);
-                                            cadData.ControlPoints.Add(new double[] { cp.X, cp.Y, cp.Z });
-                                        }
-                                    }
-
-                                    if (spline.NumFitPoints > 0)
-                                    {
-                                        cadData.FitPoints = new List<double[]>();
-                                        for (int i = 0; i < spline.NumFitPoints; i++)
-                                        {
-                                            Point3d fp = spline.GetFitPointAt(i);
-                                            cadData.FitPoints.Add(new double[] { fp.X, fp.Y, fp.Z });
-                                        }
-                                    }
-
-                                    cadData.Degree = spline.Degree;
-                                    cadData.IsRational = spline.IsRational;
-                                    cadData.IsClosed = spline.Closed;
-                                }
-                                else if (ent is Ellipse ellipse)
-                                {
-                                    cadData.CenterPoint = new double[] { ellipse.Center.X, ellipse.Center.Y, ellipse.Center.Z };
-
-                                    cadData.MajorAxis = new double[] { ellipse.MajorAxis.X, ellipse.MajorAxis.Y, ellipse.MajorAxis.Z };
-                                    cadData.RadiusRatio = ellipse.RadiusRatio;
-
-                                    cadData.StartAngle = ellipse.StartAngle;
-                                    cadData.EndAngle = ellipse.EndAngle;
-                                }
-
-                                tempList.Add(cadData);
-                            }
-                        }
-                        return tempList;
-                    }
-                    cadObjectsData_A = ExtractDataFromLists(_selectedObjectIds_A, "A");
-                    cadObjectsData_B = ExtractDataFromLists(_selectedObjectIds_B, "B");
-                    cadObjectsData_C = ExtractDataFromLists(_selectedObjectIds_C, "C");
-                    tr.Commit();
-                }
+                tr.Commit();
             }
 
             //Serialize to JSON
@@ -222,6 +136,42 @@ namespace AutoCAD_NET_4_8_Framework
             }
         }
 
+        private async void OnSendToServer_Click(object sender, EventArgs e)
+        {
+            if (_selectedObjectIds_A.Count == 0 && _selectedObjectIds_B.Count == 0 && _selectedObjectIds_C.Count == 0)
+            {
+                MessageBox.Show("Please select objects first.");
+                return;
+            }
+
+            List<CADObjectData> masterUploadList = new List<CADObjectData>();
+
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+
+            using (DocumentLock docLock = doc.LockDocument())
+            using (Transaction tr = doc.Database.TransactionManager.StartTransaction())
+            {
+                masterUploadList.AddRange(GetCadData(tr, _selectedObjectIds_A, "Group_A"));
+                masterUploadList.AddRange(GetCadData(tr, _selectedObjectIds_B, "Group_B"));
+                masterUploadList.AddRange(GetCadData(tr, _selectedObjectIds_C, "Group_C"));
+
+                tr.Commit();
+            }
+
+            if (masterUploadList.Count > 0)
+            {
+                System.Windows.Forms.Button btn = sender as System.Windows.Forms.Button;
+                string originalText = btn.Text;
+                btn.Text = "Uploading...";
+                btn.Enabled = false; // Prevent double-clicking
+
+                await UploadJsonDataAsync(masterUploadList);
+
+                btn.Text = originalText;
+                btn.Enabled = true;
+            }
+        }
+
         //Helper functions
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -248,6 +198,112 @@ namespace AutoCAD_NET_4_8_Framework
             }
 
             return new List<ObjectId>();
+        }
+
+        private async Task UploadJsonDataAsync(List<CADObjectData> payload)
+        {
+            string url = "http://10.60.161.35:3000/api/cad-upload";
+
+            try
+            {
+                string json = JsonConvert.SerializeObject(payload);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                // Send POST request
+                HttpResponseMessage response = await _client.PostAsync(url, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    MessageBox.Show($"Upload Success!\nServer replied: {responseBody}");
+                }
+                else
+                {
+                    MessageBox.Show($"Server Error: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Upload Failed: {ex.Message}");
+            }
+        }
+
+        private List<CADObjectData> GetCadData(Transaction tr, List<ObjectId> ids, string groupName)
+        {
+            List<CADObjectData> tempList = new List<CADObjectData>();
+
+            foreach (ObjectId id in ids)
+            {
+                if (id.IsErased) continue;
+                Entity ent = tr.GetObject(id, OpenMode.ForRead) as Entity;
+                if (ent == null) continue;
+
+                CADObjectData data = new CADObjectData
+                {
+                    GroupName = groupName,
+                    Handle = ent.Handle.ToString(),
+                    ObjectType = ent.GetType().Name,
+                    Layer = ent.Layer
+                };
+
+                if (ent is Line line)
+                {
+                    data.StartPoint = new double[] { line.StartPoint.X, line.StartPoint.Y, line.StartPoint.Z };
+                    data.EndPoint = new double[] { line.EndPoint.X, line.EndPoint.Y, line.EndPoint.Z };
+                }
+                else if (ent is Circle circle)
+                {
+                    data.CenterPoint = new double[] { circle.Center.X, circle.Center.Y, circle.Center.Z };
+                    data.Radius = circle.Radius;
+                }
+                else if (ent is Arc arc)
+                {
+                    data.CenterPoint = new double[] { arc.Center.X, arc.Center.Y, arc.Center.Z };
+                    data.Radius = arc.Radius;
+                    data.StartAngle = arc.StartAngle;
+                    data.EndAngle = arc.EndAngle;
+                    data.TotalAngle = arc.TotalAngle;
+                }
+                else if (ent is Polyline polyline)
+                {
+                    data.Vertices = new List<double[]>();
+                    data.Bulges = new List<double>();
+                    int vertCount = polyline.NumberOfVertices;
+                    for (int i = 0; i < vertCount; i++)
+                    {
+                        Point3d pt = polyline.GetPoint3dAt(i);
+                        data.Vertices.Add(new double[] { pt.X, pt.Y, pt.Z });
+                        data.Bulges.Add(polyline.GetBulgeAt(i));
+                    }
+                    data.IsClosed = polyline.Closed;
+                }
+                else if (ent is Spline spline)
+                {
+                    if (spline.NumFitPoints > 0)
+                    {
+                        data.FitPoints = new List<double[]>();
+                        for (int i = 0; i < spline.NumFitPoints; i++)
+                        {
+                            Point3d fp = spline.GetFitPointAt(i);
+                            data.FitPoints.Add(new double[] { fp.X, fp.Y, fp.Z });
+                        }
+                    }
+                    data.Degree = spline.Degree;
+                    data.IsRational = spline.IsRational;
+                    data.IsClosed = spline.Closed;
+                }
+                else if (ent is Ellipse ellipse)
+                {
+                    data.CenterPoint = new double[] { ellipse.Center.X, ellipse.Center.Y, ellipse.Center.Z };
+                    data.MajorAxis = new double[] { ellipse.MajorAxis.X, ellipse.MajorAxis.Y, ellipse.MajorAxis.Z };
+                    data.RadiusRatio = ellipse.RadiusRatio;
+                    data.StartAngle = ellipse.StartAngle;
+                    data.EndAngle = ellipse.EndAngle;
+                }
+
+                tempList.Add(data);
+            }
+            return tempList;
         }
 
     }
